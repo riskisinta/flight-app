b8bc580cf02241e9e0c3fe85a9cfce61b3fb2015
Riskisinta <riskisinta3@gmail.com>
2020-02-15 15:27:49 +0700

    Tambah edit user

--- includes/users.php
+++ includes/users.php
@@ -2,16 +2,56 @@
 Flight::route( 'POST /users/add', function(){
 	$db = Flight::db();
 
-	// $data = array(
-	// 	'username' => 'somenone',
-	// 	'password' => md5('password')
-	// );
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-	// $id = $db->insert('users', $data);
-	// if ($id)
-	//     echo 'user was created. Id=' . $id;
-	// else
-	//     echo 'insert failed: ' . $db->getLastError();
+
+	$data = array(
+		'username' => $username,
+		'password' => md5($password)
+	);
+
+	$id = $db->insert('users', $data);
+	if ($id)
+	  flight::redirect('users');
+	else
+	    echo 'insert failed: ' . $db->getLastError();
+});
+
+flight::route('/users/delete/@username' , function($username){
+	$db = Flight::db();
+	$db->where('username', $username);
+	if($db->delete('users'))  flight::redirect('users');
+});
+
+Flight::route( 'POST /users/edit/@username', function($username){
+	$db = Flight::db();
+
+	$username_new = $_POST['username'];
+	$password_new = $_POST['password'];
+	
+	$data = Array (
+	'username' => $username_new,
+	'password' => $password_new,
+	// 'editCount' => $db->inc(2),
+	// // editCount = editCount + 2;
+	// 'active' => $db->not()
+	// // active = !active;
+	);
+
+	$db->where ('username', $username);
+	if ($db->update ('users', $data))
+	    echo $db->count . ' records were updated';
+	else
+	    echo 'update failed: ' . $db->getLastError();
+});
+
+flight::route('/users/edit/@username' , function($username){
+	Flight::view()->set('title','Users');
+
+	Flight::render('edit-user',array(
+		'username' => $username
+		));
 });
 
 Flight::route( 'GET /users(/page/@page:[0-9]+)', function($page){
